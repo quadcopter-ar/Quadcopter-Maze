fdee0bfd09adba74056169979e39096367df5543
Taj <quadcopterar2306@gmail.com>
2021-10-02 18:30:24 -0700

    Add hole to the maze and add entrance/goal

--- Assets/Project/Scripts/Maze/HoleMaze.cs
+++ Assets/Project/Scripts/Maze/HoleMaze.cs
@@ -9,11 +9,15 @@ public enum Direction {
     WEST
 }
 
-public class Maze : MonoBehaviour
+public class HoleMaze : MonoBehaviour
 {
     [Header("Stats")]
-    [SerializeField] private int rows;
-    [SerializeField] private int cols;
+    [SerializeField] [Range(5f, 51f)] private int rows;
+    [SerializeField] [Range(5f, 51f)] private int cols;
+    [Space]
+    [SerializeField] [Range(3f, 49f)] private int holeRows;
+    [SerializeField] [Range(3f, 49f)] private int holeCols;
+    [Space]
     [SerializeField] private float wallSize;
 
     [Header("Objects")]
@@ -30,10 +34,26 @@ public class Maze : MonoBehaviour
     {
         InitializeCells();
         InitializeMaze();
-        WilsonsAlgorithm();
+        GenerateMaze();
+        DestoryWallsForEntranceAndGoal();
     }
 
-    private void WilsonsAlgorithm()
+    private void OnValidate()
+    {
+        // We want odd mazes so there's a clear center
+        if (holeRows % 2 == 0) { holeRows++; }
+        if (holeCols % 2 == 0) { holeCols++; }
+
+        // holeRows and holeCols can't be >= rows and cols
+        if (holeRows >= rows) { holeRows = rows - 2; }
+        if (holeCols >= cols) { holeCols = cols - 2; }
+
+        // We want odd mazes so there's a clear center
+        if (rows % 2 == 0) { rows++; }
+        if (cols % 2 == 0) { cols++; }
+    }
+
+    private void GenerateMaze() // Wilson's Algorithm
     {
         HashSet<Cell> notInMaze = new HashSet<Cell>(_cells);
         IEnumerator notInMazeEnumerator = notInMaze.GetEnumerator();
@@ -41,6 +61,7 @@ public class Maze : MonoBehaviour
 
         Cell startingCell = (Cell)notInMazeEnumerator.Current;
         notInMaze.Remove(startingCell);
+        notInMaze.Remove(null); // The cells in the hole will be null. Since this is a set it has no duplicates so removing null means removing all cells in the hole
 
         while (notInMaze.Count > 0)
         {
@@ -87,6 +108,39 @@ public class Maze : MonoBehaviour
         }
 
         return visitedThisWalk;
+    }
+
+    private void DestoryWallsForEntranceAndGoal()
+    {
+        // North entrance
+        Cell northEntrance = _cells[GetIndex(rows - 1, cols / 2)];
+        Destroy(northEntrance.Walls[(int)Direction.NORTH]);
+        northEntrance.Walls[(int)Direction.NORTH] = null;
+
+        // South entrance
+        Cell southEntrance = _cells[GetIndex(0, cols / 2)];
+        Destroy(southEntrance.Walls[(int)Direction.SOUTH]);
+        southEntrance.Walls[(int)Direction.SOUTH] = null;
+
+        // North goal
+        Cell northGoal = _cells[GetIndex((rows + holeRows) / 2, cols / 2)];
+        Destroy(northGoal.Walls[(int)Direction.SOUTH]);
+        northGoal.Walls[(int)Direction.SOUTH] = null;
+
+        // East goal
+        Cell eastGoal = _cells[GetIndex(rows / 2, (cols + holeCols) / 2)];
+        Destroy(eastGoal.Walls[(int)Direction.WEST]);
+        eastGoal.Walls[(int)Direction.WEST] = null;
+
+        // South goal
+        Cell southGoal = _cells[GetIndex((rows - holeRows) / 2 - 1, cols / 2)];
+        Destroy(southGoal.Walls[(int)Direction.NORTH]);
+        southGoal.Walls[(int)Direction.NORTH] = null;
+
+        // West goal
+        Cell westGoal = _cells[GetIndex(rows / 2, (cols - holeCols) / 2 - 1)];
+        Destroy(westGoal.Walls[(int)Direction.EAST]);
+        westGoal.Walls[(int)Direction.EAST] = null;
     }
 
     private void DestroyWallBetween(Cell a, Cell b)
@@ -168,6 +222,8 @@ public class Maze : MonoBehaviour
                 int index = GetIndex(r, c);
                 Cell cell = _cells[index];
 
+                if (cell == null) { continue; } // Cell is in the hole
+
                 for (int dir = 0; dir < 4; dir++) // Looping through directions
                 {
                     Cell neighbor = GetNeighbor(cell, (Direction)dir);
@@ -227,6 +283,13 @@ public class Maze : MonoBehaviour
             for (int r = 0; r < rows; r++)
             {
                 int index = GetIndex(r, c);
+
+                if (r  >= (rows - holeRows) / 2 && r <= (rows + holeRows) / 2 - 1) {
+                    if (c  >= (cols - holeCols) / 2 && c <= (cols + holeCols) / 2 - 1) {
+                        // This cell is in the hole so leave it as null
+                        continue;
+                    }
+                }
 
                 _cells[index] = new Cell(r, c, new GameObject[4]);
             }
